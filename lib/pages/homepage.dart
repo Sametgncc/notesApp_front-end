b3ff0435ae6365f6adb45b18656ab2e6b1e03783
TunaBozlak <bozlaktuna5@gmail.com>
2024-05-15 17:40:01 +0300

    .

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,224 +5,47 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Notlarım'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // Implement search functionality
-            },
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Notlarım'),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Klasörler'),
+              Tab(text: 'Tüm Notlar'),
+            ],
           ),
-          IconButton(
-            icon: Icon(Icons.more_vert),
-            onPressed: () {
-              // Implement more actions
-            },
-          ),
-        ],
-      ),
-
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 2, // İki sütun
-              mainAxisSpacing: 16.0, // Kareler arası dikey boşluk
-              crossAxisSpacing: 16.0, // Kareler arası yatay boşluk
-              childAspectRatio: 1.0, // Karelerin boyutları eşit
-              padding: EdgeInsets.all(16.0),
-              children: [
-                // Folder 1: Tüm Notlar
-                GestureDetector(
-                  onTap: () {
-                    // Tüm notları görüntülemek için ekranı aç
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => AllNotesScreen()),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Tüm Notlar',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18, // Metin boyutunu artır
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Folder 2: Kişisel Notlarım
-                GestureDetector(
-                  onTap: () {
-                    // Kişisel notlar için ekranı aç
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => PersonalNotesScreen()),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Kişisel Notlarım',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18, // Metin boyutunu artır
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Folder 3: Yapılacaklar Listesi
-                GestureDetector(
-                  onTap: () {
-                    // Yapılacaklar listesi için ekranı aç
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ToDoListScreen()),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Yapılacaklar Listesi',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18, // Metin boyutunu artır
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Folder 4: Günlük
-                GestureDetector(
-                  onTap: () {
-                    // Günlük için ekranı aç
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => JournalScreen()),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Rüya Günlüğü',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18, // Metin boyutunu artır
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Folder 5: Projeler
-                GestureDetector(
-                  onTap: () {
-                    // Projeler için ekranı aç
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ProjectsScreen()),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Projeler',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18, // Metin boyutunu artır
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-
-                // Folder 6: Okuma Listesi
-                GestureDetector(
-                  onTap: () {
-                    // Okuma listesi için ekranı aç
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => ReadingListScreen()),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey[500],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text(
-                        'Okuma Listesi',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18, // Metin boyutunu artır
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Implement search functionality
+              },
             ),
-          ),
-          // Ekle Butonu
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 35.0), // Yatay dolguyu artır
-            child: Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Ekleme işlevselliğini uygula
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.add),
-                        SizedBox(width: 12), // İkon ve metin arasındaki boşluğu biraz daha artır
-                        Text('Yeni Not Ekle', style: TextStyle(fontSize: 18)), // Metin boyutunu artır
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {
+                // Implement more actions
+              },
             ),
-          ),
-          SizedBox(height: 16), // Butonla alt arasındaki boşluğu artır
-        ],
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            ChooseFolderScreen(),
+            AllNotesScreen(),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ChooseFolderScreen()),
+            );
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
@@ -231,82 +54,99 @@ class HomePage extends StatelessWidget {
 class AllNotesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Tüm Notlar'),
-      ),
-      body: Center(
-        child: Text('Tüm Notlar Ekranı'),
-      ),
+    return Center(
+      child: Text('Tüm Notlar Ekranı'),
     );
   }
 }
 
-class PersonalNotesScreen extends StatelessWidget {
+class ChooseFolderScreen extends StatelessWidget {
+  final List<String> folders = [
+    'Kişisel Notlarım',
+    'Görev Listesi',
+    'Rüya Günlüğü',
+    'Projeler',
+    'Okuma Listesi',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Kişisel Notlarım'),
-      ),
-      body: Center(
-        child: Text('Kişisel Notlar Ekranı'),
-      ),
+    return GridView.count(
+      crossAxisCount: 2,
+      children: folders
+          .map(
+            (folder) => GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddNoteScreen(folderName: folder)),
+            );
+          },
+          child: Container(
+            margin: EdgeInsets.all(8),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey[500],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Text(
+                folder,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+          .toList(),
     );
   }
 }
 
-class ToDoListScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Yapılacaklar Listesi'),
-      ),
-      body: Center(
-        child: Text('Yapılacaklar Listesi Ekranı'),
-      ),
-    );
-  }
-}
+class AddNoteScreen extends StatelessWidget {
+  final String folderName;
 
-class JournalScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Rüya Günlüğü'),
-      ),
-      body: Center(
-        child: Text('Rüya Günlük Ekranı'),
-      ),
-    );
-  }
-}
+  const AddNoteScreen({Key? key, required this.folderName}) : super(key: key);
 
-class ProjectsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Projeler'),
-      ),
-      body: Center(
-        child: Text('Projeler Ekranı'),
-      ),
-    );
-  }
-}
+    TextEditingController noteController = TextEditingController(); // Not girişi için controller
 
-class ReadingListScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Okuma Listesi'),
+        title: Text('Yeni Not Ekle - $folderName'),
       ),
-      body: Center(
-        child: Text('Okuma Listesi Ekranı'),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: noteController, // Controller atanması
+              decoration: InputDecoration(
+                hintText: 'Notunuzu buraya yazın',
+              ),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Notu kaydetme işlevselliği
+                String note = noteController.text;
+                // Notu kaydetmek için gereken işlemler burada gerçekleştirilecek
+                // Örneğin, Firestore veya SQLite gibi bir veritabanına notu kaydetme işlemi yapılabilir
+                // Bu örnekte sadece konsola notu yazdıralım
+                print('Not kaydedildi: $note, Klasör: $folderName');
+                Navigator.pop(context); // Not ekleme ekranından çık
+              },
+              child: Text('Notu Kaydet'),
+            ),
+          ],
+        ),
       ),
     );
   }
